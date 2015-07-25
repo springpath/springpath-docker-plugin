8c4e0d8549cc8e7ed06d2ed1294d48de81cc2574
Abhijit Hoskeri <abhijit@storvisor.com>
2015-07-25 13:08:56 -0700

    request decoding and validation.

--- driver.go
+++ driver.go
@@ -5,7 +5,9 @@ import "net"
 import "log"
 import "flag"
 import "io/ioutil"
+import "encoding/json"
 
+// Global State.
 type Config struct {
 	Sock           string
 	StateFile      string
@@ -13,6 +15,7 @@ type Config struct {
 	MountBase      string
 }
 
+// Individual mounted volumes.
 type Volume struct {
 	Name          string // docker volume name
 	DatastorePath string // springpath datastore name + path
@@ -26,14 +29,33 @@ type VolumeDriver interface {
 	Remove(name string) error
 	Path(name string) (mountpoint string, err error)
 	Mount(name string) (mountpoint string, err error)
-	Unmount(name string) (mountpoint string, err error)
+	Unmount(name string) error
 }
 
-type VolumeList map[string]Volume
-
-type DockerVolumes struct {
+// Set of known Volumes.
+type VolumeMap struct {
 	VolumeDriver
-	volumes VolumeList
+	m map[string]Volume
+}
+
+func (v *VolumeMap) Create(name string) error {
+	return nil
+}
+
+func (v *VolumeMap) Remove(name string) error {
+	return nil
+}
+
+func (v *VolumeMap) Path(name string) (mountpoint string, err error) {
+	return
+}
+
+func (v *VolumeMap) Mount(name string) (mountpoint string, err error) {
+	return
+}
+
+func (v *VolumeMap) Unmount(name string) error {
+	return nil
 }
 
 const dockerPluginManifestJSON = `
@@ -49,7 +71,20 @@ func pluginActivate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(dockerPluginManifestJSON))
 }
 
-func pluginHandler(w http.ResponseWriter, r *http.Request) {
+type PluginHandler struct {
+	http.Handler
+	VMap *VolumeMap
+	Fn   interface{} // sigh.
+}
+
+type Message struct {
+	Name       string `json:"omitempty"`
+	Err        string `json:"omitempty"`
+	Mountpoint string `json:"omitempty"`
+}
+
+// Common portions of all the plugin endpoints.
+func (h PluginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var requestPath = r.URL.Path
 
 	defer r.Body.Close()
@@ -61,33 +96,42 @@ func pluginHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("req %p: url %s, body %s\n", r, requestPath, requestBody)
 
-	// Until we write all handlers.
-	w.WriteHeader(http.StatusNotFound)
+	var request Message
+	json.Unmarshal(requestBody, &request)
+
+	return
 }
 
 func main() {
-	v := new(Config)
+	config := new(Config)
 
 	// parse command line.
-	flag.StringVar(&v.Sock, "sockpath", "/run/docker/plugins/springpath.sock", "unix domain socket docker talks to")
-	flag.StringVar(&v.StateFile, "statefile", "/SYSTEM/volume-driver.json", "springpath volume driver metadata")
-	flag.StringVar(&v.ClusterAddress, "clusteraddress", "localhost", "address of the springpath i/o dispatcher")
-	flag.StringVar(&v.MountBase, "mountbase", "/run/springpath-docker-volumes", "base path for springpath volume mount points")
+	flag.StringVar(&config.Sock, "sockpath", "/run/docker/plugins/springpath.sock", "unix domain socket docker talks to")
+	flag.StringVar(&config.StateFile, "statefile", "/SYSTEM/volume-driver.json", "springpath volume driver metadata")
+	flag.StringVar(&config.ClusterAddress, "clusteraddress", "localhost", "address of the springpath i/o dispatcher")
+	flag.StringVar(&config.MountBase, "mountbase", "/run/springpath-docker-volumes", "base path for springpath volume mount points")
 	flag.Parse()
 
 	log.Println("starting docker volume plugin")
 
+	volMap := new(VolumeMap)
+
 	// http server.
 	http.HandleFunc("/Plugin.Activate", pluginActivate)
-	http.HandleFunc("/", pluginHandler)
+	http.Handle("/VolumeDriver.Create", PluginHandler{Fn: volMap.Create, VMap: volMap})
+	http.Handle("/VolumeDriver.Remove", PluginHandler{Fn: volMap.Create, VMap: volMap})
+	http.Handle("/VolumeDriver.Mount", PluginHandler{Fn: volMap.Create, VMap: volMap})
+	http.Handle("/VolumeDriver.Unmount", PluginHandler{Fn: volMap.Create, VMap: volMap})
+	http.Handle("/VolumeDriver.Path", PluginHandler{Fn: volMap.Create, VMap: volMap})
 
-	listener, err := net.Listen("unix", v.Sock)
+	// XXX: set SO_REUSEADDR
+	listener, err := net.Listen("unix", config.Sock)
 
 	if err != nil {
 		log.Fatal("failed to start http server", err)
 	}
 
-	log.Println("springpath volume driver listening on", v.Sock)
+	log.Println("springpath volume driver listening on", config.Sock)
 	err = http.Serve(listener, http.DefaultServeMux)
 	if err != nil {
 		log.Fatal("http serve failed", err)
